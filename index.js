02b827c784f99d97f6db4ff5368b3397a6d562b2
Yury Misiyevich <vinchesterok@tut.by>
2019-08-08 19:18:13 +0300

    feat:add bucket, debug, begin style changes

--- index.js
+++ index.js
@@ -34,6 +34,9 @@ window.onload = function(){
         drawing = new Array(w+2);
         for(let k = 0; k < drawing.length; k++){
           drawing[k]=new Array(h+2);
+          for(let i = 0;i<h+2; i++){
+            drawing[k][i]=' ';
+          }
         }
         for(let k = 0; k <= w+1; k++){
           drawing[k][0]='-';
@@ -44,58 +47,122 @@ window.onload = function(){
           drawing[w+1][k]='|';
         }
       }
+      
       //line
       if(command[0]==='L'){
-        //horizontal line
-        if(command[1]===command[3]){
-          let x = +command[1];
-          let y1 = Math.min(+command[2],+command[4]);
-          let y2 = Math.max(+command[2],+command[4]);
-          for (let y = y1; y<=y2;y++){
-            drawing[x][y]='x';
-          }
-        }
-        //vertical line
-        else{
-          let y = +command[2];
-          let x1 = Math.min(+command[1],+command[3]);
-          let x2 = Math.max(+command[1],+command[3]);
-          for (let x = x1; x<=x2;x++){
-            drawing[x][y]='x';
-          }
-        }
+        liner(+command[1],+command[2],+command[3],+command[4]);
       }
       
       //rectangle
       if(command[0]==='R'){
-      
-      
+        let x1 = +command[1];
+        let y1 = +command[2];
+        let x3 = +command[3];
+        let y3 = +command[4];
+        let x2 = x1;
+        let y2 = y3;
+        let x4 = x3;
+        let y4 = y1;
+        liner(x1,y1,x2,y2);
+        liner(x1,y1,x3,y3);
+        liner(x2,y2,x4,y4);
+        liner(x3,y3,x4,y4);
       }
       
-      
+      //bucket
+      if(command[0]==='B'){
+    
+
+        let x = +command[1];
+        let y = +command[2];
+        let color = command[3];
+        let initialColor = drawing[+command[1]][+command[2]];
+        let pixelStack=[[x,y]];  //stack for vertical lines for coloring
+        
+        while(pixelStack.length){
+          let newxy = pixelStack.pop();
+          let reachLeft = false;
+          let reachRight = false;
+          x = newxy[0];
+          y = newxy[1];
+          let currentPixelColor = drawing[x][y];
+          while(y>0 && currentPixelColor===initialColor ){
+            currentPixelColor = drawing[x][y-1];
+            y--;
+          }
+          //reset
+          ++y;
+          currentPixelColor = drawing[x][y];
+          
+          while (y++ <= h && currentPixelColor===initialColor){
+            drawing[x][y-1]=color;
+            //move left
+            if(x>1){
+              if(drawing[x-1][y-1]===initialColor){
+                if(!reachLeft){
+                  pixelStack.push([x-1,y-1]);
+                  reachLeft = true;
+                }
+              }
+              else if(reachLeft){
+                reachLeft = false;
+              }
+            }
+            
+            //move right
+            if(x < w){
+              if(drawing[x+1][y-1]===initialColor){
+                if(!reachRight){
+                  pixelStack.push([x+1,y-1]);
+                  reachRight = true;
+                }
+              }
+              else if(reachRight){
+                  reachRight = false;
+              }
+            }
+            currentPixelColor = drawing[x][y];
+            
+          }
+        }
+        
+      }
     });
   
+
+    
   
-  
+    //line drawer
+    function liner(x1,y1,x2,y2){
+      //horizontal line
+      if(x1===x2){
+        let x = x1;
+        for (let y = Math.min(y1,y2); y<=Math.max(y1,y2);y++){
+          drawing[x][y]='x';
+        }
+      }
+      //vertical line
+      else{
+        let y = y1;
+        for (let x = Math.min(x1,x2); x<=Math.max(x1,x2);x++){
+          drawing[x][y]='x';
+        }
+      }
+    };
   
     //create canvas
     let canvas='';
     for (let y = 0;y <= h+1; y++){
       for(let x = 0;x <= w+1;x++){
-        if(drawing[x][y]){
           canvas+=drawing[x][y];
-        }
-        else{
-          canvas+=' ';
-        }
       }
       canvas+='\n';
     }
-    console.log(canvas);
+
     
-    /*
-    let blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
-    saveAs(blob, "hello world.txt");
-    */
+    
+    let blob = new Blob([canvas], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "output.txt");
+    
   }
 };
